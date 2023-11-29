6af999447c2d9698a7f52d6f889bc1186658c475
Gardak <a.lorrain@hotmail.com>
2023-11-28 20:02:40 -0500

    Added php setup
    started to set a push function of the data towards MongoDB

--- index.php
+++ index.php
@@ -1,3 +1,65 @@
+<?php
+
+//Post a file into mongoDB
+//Function Called when pressing the add Task button from the script.js
+if($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+    echo("Connected to the collection");
+
+
+        //Data needed to be posted unto the mongoDB collection
+        $taskName = $_POST['task_name'];
+
+     if($_FILES)
+     {
+
+        
+        $fname = $_FILES['filename']['name'];
+        move_uploaded_file($_FILES['filename']['tmp_name'], "images/".$fname);
+
+        try {
+            require_once __DIR__ . '/vendor/autoload.php';
+        
+            //Connect to the mongoDB collection
+            $client = 
+            new MongoDB\Client(
+                "mongodb+srv://jaebrn:NCGJwCSrV2DN7HSb@cart351-jbrown.gpkovlg.mongodb.net/?retryWrites=true&w=majority"
+            
+            );
+            $collection = $client->CART351->GalleryItems;
+        
+                   
+            //Determine how the data is diplayed in the mongoDB collection
+            $collection->insertOne(
+                ['Task Name' => $taskName
+                ]);
+            
+            //Encode the data as a JSON for mongoDB
+            $data = array();
+            $data["response"] = "success";
+                echo json_encode($data);
+                exit;
+            }
+
+            //End the TRY()
+            catch (Exception $e) {
+                echo 'Caught exception: ',  $e->getMessage(), "\n";
+            }
+
+            //Confirm the connection is set to mongoDB
+        echo("Connected to the collection");
+        echo("<br>");
+
+    exit;
+    }
+}
+
+
+
+
+
+?>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,8 +73,9 @@
         <!-- To-Do List Items will be inserted here dynamically -->
     </div>
 
-    <div id="addTaskContainer">
-        <input type="text" id="taskInput" placeholder="Add a new task">
-        <button onclick="addTask()">Add Task</button>
-    </div>
+   
+        <div id="addTaskContainer">
+            <input type="text" id="taskInput" placeholder="Add a new task" name="task_name">
+            <button onclick="addTask()">Add Task</button>
+        </div>
 </body>
--- script.js
+++ script.js
@@ -1,14 +1,42 @@
 function addTask() {
-    const taskInput = document.getElementById('taskInput');
+    
+    let taskInput = document.querySelector("#taskInput");
     const taskText = taskInput.value.trim();
+    console.log(taskInput);
 
     if (taskText !== '') {
         // Post new task to the server
 
+        //Pakage the data into KeyValue pairs to send it
+        let taskData = new FormData(taskInput);
+
         // Clear input field
-        console.log(taskText);
         taskInput.value = '';
+
     }
+
+    //Grab the taskData value and confirm it has been sent to the collection
+    fetch('/script.js', {
+        method: 'POST',
+        body: taskData
+        })
+        .then(result => {
+ 
+            console.log(result.response);
+            if(result.response!=="success"){
+                throw new Error('Something went wrong '+ result);
+     
+            }
+            else{
+                console.log('Here:', result);
+                taskInput.reset();
+     
+            }
+         })
+        .catch(error => {
+        console.error('Error:', error);
+        });
+
 }
 
 function updateTaskUI(tasks) {
